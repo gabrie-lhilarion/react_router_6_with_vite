01bc304f0d1b9abd1f7fd07b061253f7243a18d9
gabriel <gabrielhilarion@gmail.com>
2023-09-10 03:25:24 +0100

    Create contacts

--- contacts_app/src/main.jsx
+++ contacts_app/src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+import Root, { 
+  loader as rootLoader, 
+  action as rootAction,
+
+} from "./routes/root";
+
+import ErrorPage from "./error-page";
+
+import Contact,{
+  loader as contactLoader,
+} from "./routes/contact";
+
+import EditContact from "./routes/edit";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+      },
+    ]
+  },
+    
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
